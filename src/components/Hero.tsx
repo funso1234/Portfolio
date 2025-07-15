7741d9eefda7f0bc2afbffab1734663a328db157
funso1234 <samuel08063251499@gmail.com>
2025-07-15 21:35:25 +0100

    work in progress

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
-import Sam from '../assets/Sammy.jpg'
+
+
 
 const Hero = () => (
   <section className="pt-32 pb-20 relative overflow-hidden">
@@ -39,7 +40,7 @@ const Hero = () => (
 
     
           <img
-            src={Sam}
+            src="https://res.cloudinary.com/dhwswtcb6/image/upload/v1752611577/Sammy_v7ngyn.jpg"
             alt="Samuel Oyewusi"
             className="relative rounded-3xl w-full h-auto border-2 border-cyan-500/20"
           />
